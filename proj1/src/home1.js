58e2e9b74cfd912b055f839b9a7924d5337a3961
Vlad <anoyanvlad@gmail.com>
2017-06-14 18:50:30 +0400

    homework12

--- proj1/src/home1.js
+++ proj1/src/home1.js
@@ -5,54 +5,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Input from './input';
 
-let m=0;
 class App1 extends Component {
     constructor(props){
         super(props);
         this.state={
-            hover:false,
-            hover1:false,
-            hover2:false,
-            hover3:false,
-            hover4:false,
-            hover5:false,
-            hover6:false,
-            hover7:false,
-            hover8:false,
-            hover9:false,
-            hover10:false,
-            hover11:false,
-            hover12:false,
-            hover13:false,
-            hover14:false
+
         }
     }
 
 
     render() {
 
-        if( document.getElementsByClassName("hover").clicked==true )
-        {
-            m=m+1
-        }
-        console.log(m);
+
         return (
             <div className="App1">
-                <div onClick={() => this.setState({ hover: !this.state.hover })} className={ this.state.hover ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover1: !this.state.hover1 })} className={ this.state.hover1 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover2: !this.state.hover2 })} className={ this.state.hover2 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover3: !this.state.hover3 })} className={ this.state.hover3 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover4: !this.state.hover4 })} className={ this.state.hover4 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover5: !this.state.hover5 })} className={ this.state.hover5 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover6: !this.state.hover6 })} className={ this.state.hover6 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover7: !this.state.hover7 })} className={ this.state.hover7 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover8: !this.state.hover8 })} className={ this.state.hover8 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover9: !this.state.hover9 })} className={ this.state.hover9 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover10: !this.state.hover10 })} className={ this.state.hover10 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover11: !this.state.hover11 })} className={ this.state.hover11 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover12: !this.state.hover12 })} className={ this.state.hover12 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover13: !this.state.hover13 })} className={ this.state.hover13 ? 'hover1' : 'hover'}></div>
-                <div onClick={() => this.setState({hover14: !this.state.hover14 })} className={ this.state.hover14 ? 'hover1' : 'hover'}></div>
+
 
                 <Input />
             </div>
--- proj1/src/input.js
+++ proj1/src/input.js
@@ -4,16 +4,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Modal from './modal';
-
+let arr=[];
+console.log(arr);
 
 class App1 extends Component {
     constructor(props){
         super(props);
         this.state={
+            search1:false,
             value1:"",
             clicked:false,
             text1:"",
             color:"mymodalh1",
+            show:false,
+            hid:false
 
 
 
@@ -32,19 +36,47 @@ class App1 extends Component {
         }
     }
 
+    checked()
+    {
+        this.setState({search1: !this.state.search1})
+    }
+
+
+
     valuechek(event){
-            this.setState({value1:event.target.value})
+       this.setState({value1:event.target.value});
 
     }
     cloes(){
         this.setState({clicked:false})
     }
+
+    sho(){
+        this.setState({show:true})
+
+    }
+
+    hid(){
+        this.setState({hid:true})
+    }
+    copy() {
+        arr.push(this.state.value1);
+        let m = document.getElementsByClassName("container")[0];
+        m.innerHTML = arr;
+        this.setState({value1: ""});
+    }
     render() {
 
 return (
             <div className="App1">
+                <input className="chek"  type="checkbox" onChange={this.checked.bind(this)}/>
+                <input className={this.state.search1 ? 'search1' : 'search'}  type="text" onChange={this.valuechek.bind(this)} value={this.state.value1} placeholder="YOUR TEXT HERE" />
+                <input className="chek"  type="radio" name="one" onChange={this.sho.bind(this)}/>
+                <input className="chek"  type="radio" name="two" onChange={this.hid.bind(this)}/>
+                <button className={this.state.hid ? "hidbat":"hidbat1"} onClick={this.copy.bind(this)}>push</button>
 
-                <input type="text" onChange={this.valuechek.bind(this)} value={this.state.value1} />
+                <div className={this.state.show ? 'container' : 'container1'}></div>
+                <br/>
                 <button onClick={this.check.bind(this)}>check </button>
                 <Modal modal={this.state.clicked ? 'modal1' : 'modal'} text={this.state.text1} color={this.state.color} open={this.cloes.bind(this)} />
             </div>
