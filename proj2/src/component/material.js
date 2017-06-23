88ce48338bd925f45436068648d54148ee70243f
Vlad <anoyanvlad@gmail.com>
2017-06-23 19:21:51 +0400

    asasd

--- proj2/src/component/material.js
+++
@@ -1,9 +0,0 @@
-
-import React, { Component } from 'react';
-import RaisedButton from "material-ui/RaisedButton";
-
-const  Material = () => (
-  <RaisedButton label ="default"/>
-);
-
-export default Material;
